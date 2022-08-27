81bb213ff90ab0325447763dcd7ee150704106ad
leokingue <ebangkingue@icloud.com>
2022-08-27 12:31:27 -0700

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 1
     b = 2
     printf("{} + {} = {}".format(a, b, add(a, b)))
-    printf("{} + {} = {}".format(a, b, sub(a, b)))
-    printf("{} + {} = {}".format(a, b, mul(a, b)))
-    printf("{} + {} = {}".format(a, b, div(a, b)))
+    printf("{} - {} = {}".format(a, b, sub(a, b)))
+    printf("{} * {} = {}".format(a, b, mul(a, b)))
+    printf("{} / {} = {}".format(a, b, div(a, b)))
