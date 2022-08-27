7150401fee05e47d451914b2c2e200042b71e257
leokingue <ebangkingue@icloud.com>
2022-08-27 12:55:52 -0700

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, ml, div
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+
 
     a = 10
     b = 5
-    printf("{} + {} = {}".format(a, b, add(a, b)))
-    printf("{:d} - {:d} = {}".format(a, b, sub(a, b)))
-    printf("{} * {} = {}".format(a, b, mul(a, b)))
-    printf("{} / {} = {}".format(a, b, div(a, b)))
+    printf("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    printf("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    printf("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    printf("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
