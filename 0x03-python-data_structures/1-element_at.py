738ccae8da6ed7140b718788c2516e547911c152
leokingue <ebangkingue@icloud.com>
2022-08-30 13:40:11 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 def element_at(my_list, idx):
-    return (my_list[idx] if 0 <= idx < len(my_list) else None)
+    """
+    my_list: a list
+    @idx: index of element
+    Return: an element of list
+    """
+    if (idx < 0) or (idx > len(my_list) - 1):
+        return None
+    else:
+        return my_list[idx]
