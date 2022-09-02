50f202a752a31ac8268146a28070f32e384db86a
leokingue <ebangkingue@icloud.com>
2022-09-02 08:37:30 -0700

    update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace):
     def serch_element(element):
         return element if search != element else replace
-    return [list(map(search_element, my_list))]
+    return list(map(search_element, my_list))
