773998184c9a542a2cc6ff55222da4e0cafde48b
leokingue <ebangkingue@icloud.com>
2022-09-27 17:30:02 -0700

    update

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+class MyList(list):
+    """My own class list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """Method that displays the sorted elements"""
+        print(sorted(sort))
