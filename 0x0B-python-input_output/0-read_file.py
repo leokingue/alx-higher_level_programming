b507a681d51a07e195cd981728d7d708b298ad66
leokingue <ebangkingue@icloud.com>
2022-10-02 13:15:00 -0700

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Contents of the read_file definition
 def read_file(filename=""):
     """Read file function"""
     with open(filename, "r", encoding="utf-8") as new_fil:
-        print(new_fil.read())
+        print(new_fil.read(), end="")
