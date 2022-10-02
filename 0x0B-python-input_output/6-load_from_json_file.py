5bed15b419ceb5d955e562311922f15be20a685c
leokingue <ebangkingue@icloud.com>
2022-10-02 14:27:05 -0700

    update

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Create object from a JSON file
+"""
+
+
+import json
+
+def load_from_json_file(filename):
+    """load from json file function"""
+    with open(filename, "r", encoding="utf-8") as new_fil:
+        json.load(new_fil)
