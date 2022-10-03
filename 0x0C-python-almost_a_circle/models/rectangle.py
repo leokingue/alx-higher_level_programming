6510c3ad5ff3509a86384f558b82401c45983b43
leokingue <ebangkingue@icloud.com>
2022-10-03 07:32:54 -0700

    fix the getter and setter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,32 +16,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
+        """getter height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """getter x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter x """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
+    """getter y"""
     def y(self):
         return self.__y
 
     @y.setter
+    """"setter y"""
     def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
         self.__y = value
