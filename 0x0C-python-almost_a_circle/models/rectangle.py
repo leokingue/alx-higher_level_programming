50dcbd2b6f9892ddb892741c684e70987ed89476
leokingue <ebangkingue@icloud.com>
2022-10-03 15:00:26 -0700

    fix the update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,17 +89,28 @@ class Rectangle(Base):
                                                                  self.__width,
                                                                  self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """update the attributes"""
         if len(args):
             for i, a in enumerate(args):
                 if i == 0:
-                    self.__id = a
+                    self.id = a
                 elif i == 1:
-                    self.__width = a
+                    self.width = a
                 elif i == 2:
-                    self.__height = a
+                    self.height = a
                 elif i == 3:
-                    self.__x = a
+                    self.x = a
                 elif i == 4:
-                    self.__y = a
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
