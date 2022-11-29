b2278fa7ce032a838399f681e7712015f3cc2c4f
leokingue <ebangkingue@icloud.com>
2022-11-29 11:30:18 -0800

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,17 +1,4 @@
 #!/usr/bin/node
-const language = ['C', 'Python', 'Javascript'];
-for (let i = 0; i < language.length; ++i) {
-  switch (i) {
-    case 0:
-      console.log(language[0] + ' is fun');
-      break;
-    case 1:
-      console.log(language[1] + ' is cool');
-      break;
-    case 2:
-      console.log(language[2] + ' is amazing');
-      break;
-    default:
-      break;
-  }
-}
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
