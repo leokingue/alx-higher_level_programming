465a4d853667a6b10072c8dcce3cbd5c50a90175
leokingue <ebangkingue@icloud.com>
2022-12-08 13:21:23 -0800

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
             " '{:s}' ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
