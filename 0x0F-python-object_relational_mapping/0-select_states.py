497b1e8248de043740606243d5f0ee90aa42c0c2
leokingue <ebangkingue@icloud.com>
2022-12-08 12:57:10 -0800

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,7 @@ from the database
 '''
 
 if __name__ == "__main__":
+    '''select all states'''
     con = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         passwd=argv[2], db=argv[3])
